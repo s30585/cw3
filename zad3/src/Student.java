2453d5e1b3d40ed3653efa82c9f4daf4d6d098bd
Oliwia Kielak <s30585@pjwstk.edu.pl>
2025-03-20 13:06:42 +0100

    test

---
+++ zad3/src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    String fname;
+    String lname;
+    int indexNumber;
+    String email;
+    String address;
+    int grade;
+    public Student(String fname, String lname, int indexNumber, String email, String address, int grade) {
+        this.fname = fname;
+        this.lname = lname;
+        this.indexNumber = indexNumber;
+        this.email = email;
+        this.address = address;
+        this.grade = grade;
+    }
+}
