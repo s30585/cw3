2a4928ca3a3724ae9044846bda9b82ab5cc8fdbc
Oliwia Kielak <s30585@pjwstk.edu.pl>
2025-03-20 13:38:19 +0100

    zad

--- zad3/src/Student.java
+++ zad3/src/Student.java
@@ -1,16 +1,27 @@
 public class Student {
-    String fname;
-    String lname;
-    int indexNumber;
-    String email;
-    String address;
-    int grade;
-    public Student(String fname, String lname, int indexNumber, String email, String address, int grade) {
+    public String fname;
+    public String lname;
+    public String indexNumber;
+    public String email;
+    public String address;
+    public int grade;
+    public Student(String fname, String lname, String indexNumber, String email, String address, int grade) {
         this.fname = fname;
         this.lname = lname;
         this.indexNumber = indexNumber;
         this.email = email;
         this.address = address;
         this.grade = grade;
+    }
+    public double calculateAverage(double srednia) {
+        if (grade == 2
+        || grade == 2.5
+        || grade == 3
+        || grade == 3.5
+        || grade == 4
+        || grade == 4.5
+        || grade == 5) {
+
+        }
     }
 }
--- zad3/src/Studentgroup.java
+++ zad3/src/Studentgroup.java
@@ -1,3 +1,6 @@
 public class Studentgroup {
-    
+   public String nazwa;
+    Studentgroup(String nazwa) {
+        this.nazwa = nazwa;
+    }
 }
