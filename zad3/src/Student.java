d0ddf957c5a4f0432d2ab33f0bbb2707cd17159e
s30585 <s30585@pjwstk.edu.pl>
2025-03-26 21:42:57 +0100

    Update Student.java

--- zad3/src/Student.java
+++ zad3/src/Student.java
@@ -4,24 +4,34 @@ public class Student {
     public String indexNumber;
     public String email;
     public String address;
-    public int grade;
-    public Student(String fname, String lname, String indexNumber, String email, String address, int grade) {
+    double[] grades = new double[20];
+    int gradeCount = 0;
+
+    public Student(String fname, String lname, String indexNumber, String email, String address) {
         this.fname = fname;
         this.lname = lname;
         this.indexNumber = indexNumber;
         this.email = email;
         this.address = address;
-        this.grade = grade;
     }
-    public double calculateAverage(double srednia) {
-        if (grade == 2
-        || grade == 2.5
-        || grade == 3
-        || grade == 3.5
-        || grade == 4
-        || grade == 4.5
-        || grade == 5) {
-
+    void grades(double grade) {
+        if (grade < 2.0 || grade > 5.0 || (grade *10) % 5 != 0) {
+            throw new IllegalArgumentException("Niepoprawna ocena");
         }
+        if (gradeCount >= 20) {
+            throw new IllegalArgumentException("Maksymalnie student może mieć 20 ocen");
+        }
+        grades[gradeCount++] = grade;
+    }
+
+    double getAverage() {
+        if (gradeCount == 0) {
+            throw new IllegalArgumentException("Student musi mieć co najmniej jedną ocenę");
+        }
+        double suma = 0;
+        for (int i = 0; i < gradeCount; i++) {
+            suma += grades[i];
+        }
+        return (Math.round((suma/gradeCount)*2))/2.0;
     }
 }
